e5e238b1d9e561ee147f45f22d0ea27a4ce3b528
Paul Beaudet <inof8or@gmail.com>
2014-02-12 12:20:46 -0500

    fixed button interupt bug

--- button.ino
+++ button.ino
@@ -1,6 +1,8 @@
 
 #define ZUMOBUTTON 12//define the pin that the zumo button is on
 
+// ##############  Core Functions
+
 void buttonUp()// it's cold out there
 { 
   pinMode(ZUMOBUTTON, INPUT);//set the zumo button as an input
@@ -9,14 +11,16 @@ void buttonUp()// it's cold out there
 
 #define BOUNCETIME 10//ms anytime grater is to know the buton has settled
 // wait this long to be sure of a legit press
-#define HOLDSTATE 400//ms
+#define HOLDSTATE 200//ms
 
-byte pressEvent()// all the jouralist come
+byte pressEvent(boolean type = LOW)// argument is assumed to be a press event
+//when the true argument is passed high is tested to detect a release
+// type assumes HIGH = 1 and LOW = 0 in the form of a boolean argument
 {//checks for a debounced button press event
   static unsigned long time = millis();
   static boolean timingState = false;
-
-  if ( digitalRead(ZUMOBUTTON) == LOW ) // low is an event with the pullup
+  
+  if ( digitalRead(ZUMOBUTTON) == type ) // low is a press with the pullup
   {// if the button has been pressed
     if(timingState)
     { // given the timer has started
@@ -40,8 +44,6 @@ byte pressEvent()// all the jouralist come
   return 0;//not pressed
 }
 
-
-
 void holdForButton()
 {
   while(!pressEvent())
@@ -50,37 +52,37 @@ void holdForButton()
   }
 }
 
-void intiateCalibration()
-{  
-  holdForButton(); // wait for a first press before getting too excited
-  if (preSession(RESETKEY))//previous session returns true or false
-  {
-    printResults();// Prints the results of the last session if it existed
-    rememberCalibration();// write the old calibration from EEPROM 
-  }
-  else
-  {// calibrate the compass if previous calibration is forgoton
-    calibrateCompass();
-  };
-}
+// ####################  less general opperations
 
+// buttonInterupt is to signal four series of presses
+// rotating through the four possible return states 0-3
 
 byte buttonInterupt()// returns actionable situations 
 {//monitors button (0-start music->1-start motors->2-stop music->3-stop motors->0)
   static boolean toggelMusic = 0;//1=true, 0=false
   static boolean toggelMotor = 0;// for a total of 4 cases
+  static boolean pressWait = true; //default to wait for a press
+  static byte lastEvent = 0; // hold on to the event type
+  
+  if (!pressWait && pressEvent(HIGH))// note that checking the detection state first
+  {//is very important,-> only check release in the case of a press
+    pressWait=true;// signal that another press can be accepeted
+  }// if !pressWait is basically saying besure pressWait is false
 
-  if ( pressEvent() ) // monitors for button press event
-  {
-    if (toggelMusic)
+  if ( pressWait && pressEvent() )// only check press in case of a release
+  {// monitors for hold button press event and if waiting for a press
+    pressWait = false;// signal that now a release is expected
+    if (toggelMusic) // if the music is playing
     {
-      if (toggelMotor)
+      if (toggelMotor) // if the motors are running
       {//Music and motors are going! 
         toggelMusic = false;//turn the music off
+        lastEvent=3;
         return 3;//move to situation 3: only motors
       }
       //only music is playing
       toggelMotor = true;//turn on the motors
+      lastEvent = 2;
       return 2;//move to situation 2 where the motors are on
     }
     else// if the music is off
@@ -88,38 +90,23 @@ byte buttonInterupt()// returns actionable situations
       if (toggelMotor)
       {//The music has been turned off and the motors are still on
         toggelMotor = false;//now both are false again
+        lastEvent = 0;
         return 0;//move to situation 0 to complete the loop
       } 
       //both bools are false 
       toggelMusic = true;//turn the music on!
+      lastEvent = 1;
       return 1;//move to situation 1
     };
-  }// in this way music is on in situations 1 && 2
-  //and motors are on in 2 && 3, neither are on in 0
-  // the following returns the current situation given no button press
-  if (toggelMusic)
-  {
-    if (toggelMotor)
-    {
-      return 2;
-    }
-    else
-    {
-      return 1;
-    };
   }
-  else// if the music is off
-  {
-    if (toggelMotor)
-    {// and the motors are on
-      return 3;
-    }
-    else
-    {
-      return 0;
-    }
-  };
+  return lastEvent; // In the case of no changes return the last event
+  // in this way music is on in situations 1 && 2
+  //and motors are on in 2 && 3, neither are on in 0
 }
+
+
+
+
 
 
 
