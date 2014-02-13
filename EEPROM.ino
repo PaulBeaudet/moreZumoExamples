3f4fac523be5f1d2ff06dc77b9c873d298b587db
Paul Beaudet <inof8or@gmail.com>
2014-02-13 11:46:19 -0500

    prepared for offset info

--- EEPROM.ino
+++ EEPROM.ino
@@ -1,13 +1,24 @@
 #include <EEPROM.h> // Persitent EEPROM memory storage (runtime persistence)
 
-#define SESSIONPLACE 100
+// Database ledgend // outlines addresses of persistent runtime data 
+// amount of addressable space on the uno and leonardo - 1
+#define EEPROMSIZE 1024 
+#define KEYADDRESS 12 //place where the session key is stored
+//Sample space to address starts after key address
+
+//0-8 = compass calibration information (int)
+//9-10 = Motor offset 0-400 (int)
+// 11 = weak side 0 = right / 1+ = left
+// 12 key
+
 
 void promWrite(byte data)
-{
-  static int index= 8; //start at 8 given 0-7 is taking by calibration data
+{//meant for writting debugging samples to the eeprom
+  static int index= KEYADDRESS + 1;// plus one is the next availible space
+  //start past inportant calibration values
 
-  if (index < SESSIONPLACE)
-  {
+  if (index < EEPROMSIZE)
+  {//besure not to write outside of the scope of the space availble
     EEPROM.write( index, data);
     index++;
   }
@@ -51,13 +62,13 @@ void printResults()//runs in setup
 
 boolean preSession(byte key)
 {
-  if ( EEPROM.read(SESSIONPLACE) == key )
+  if ( EEPROM.read(KEYADDRESS) == key )
   {//location is arbitrary, choose around usespace
     return true;   
   } 
   else
   {
-    EEPROM.write(SESSIONPLACE, key);//if its not it is now!!
+    EEPROM.write(KEYADDRESS, key);//if its not it is now!!
     return false;
   };
 }
