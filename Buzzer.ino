e4388a0cdefdc4fdc7e644df82137622a2058a77
paulBeaudet <inof8or@gmail.com>
2016-02-13 18:21:29 -0500

    modernize code

--- Buzzer.ino
+++ Buzzer.ino
@@ -7,7 +7,7 @@ ZumoBuzzer buzzer;// set the zumo buzzer as "buzzer"
 #define MELODY_LENGTH 95
 
 // the notes to 
-prog_uchar const note[MELODY_LENGTH] PROGMEM = {
+const int note[MELODY_LENGTH] = {
   NOTE_E(5), SILENT_NOTE, NOTE_E(5), SILENT_NOTE, NOTE_E(5), SILENT_NOTE, NOTE_C(5), NOTE_E(5),
   NOTE_G(5), SILENT_NOTE, NOTE_G(4), SILENT_NOTE,
 
@@ -29,7 +29,7 @@ prog_uchar const note[MELODY_LENGTH] PROGMEM = {
   SILENT_NOTE, NOTE_E_FLAT(5), SILENT_NOTE, NOTE_D(5), NOTE_C(5)
   };
 
-PROGMEM const prog_uint16_t duration[MELODY_LENGTH] = {
+const int duration[MELODY_LENGTH] = {
   100, 25, 125, 125, 125, 125, 125, 250, 250, 250, 250, 250,
 
   375, 125, 250, 375, 250, 250, 125, 250, 167, 167, 167, 250, 125, 125,
@@ -53,7 +53,7 @@ void marioInProgress(){
   
   if (currentIdx < MELODY_LENGTH && !buzzer.isPlaying()){
     // play note at max volume
-    buzzer.playNote(pgm_read_byte_near(&(note[currentIdx])), pgm_read_word_near(&(duration[currentIdx])), 15);
+    buzzer.playNote(note[currentIdx], duration[currentIdx], 15);
     currentIdx++;
   } else if (currentIdx == MELODY_LENGTH) { currentIdx = 13; } // reset song
 }
